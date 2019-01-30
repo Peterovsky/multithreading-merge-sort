574e0e4433c25f28d9a6088472585fcc41309ee1
Peterovsky <Peterovsky7@gmail.com>
2019-01-30 14:09:49 +0100

    A few tweaks of results output.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     // Method that check if the array is sorted
     private static boolean isArraySorted(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i+1]) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
         }
@@ -17,7 +17,8 @@ public class Main {
     public static void main(String[] args) {
 
         Random rand = new Random();
-        int[] example = new int[10000000];
+        int array_size = 10000000;
+        int[] example = new int[array_size];
         int availableThreads = getRuntime().availableProcessors(); // Amount of available processors
 
         // Filling array with random integers in range 0-99
@@ -27,22 +28,24 @@ public class Main {
 
         int[] exampleCopy = example.clone();    // Copy of array to sort
 
+        System.out.println("Array size: " + array_size + "\nAvailable threads: " + availableThreads);
+
         // Sorting using multi-threading
         MergeSort mergeSort1 = new MergeSort(example, 0, example.length - 1, availableThreads);
         long start = System.currentTimeMillis();
         mergeSort1.run();
         long elapsedTime = System.currentTimeMillis() - start;
-        System.out.println("Sorting time using " + availableThreads + " threads: " + elapsedTime);
+        System.out.println("\nSorting time using " + availableThreads + " threads: " + elapsedTime + " ms");
 
         // Sorting using single thread
         MergeSort mergeSort2 = new MergeSort(example, 0, example.length - 1);
         start = System.currentTimeMillis();
         mergeSort2.mergeSortST(exampleCopy, 0, exampleCopy.length - 1);
         elapsedTime = System.currentTimeMillis() - start;
-        System.out.println("Sorting time using single thread: " + elapsedTime);
+        System.out.println("Sorting time using single thread: " + elapsedTime + " ms");
 
-        System.out.println("Was multi-threading sorting done correctly?: " + isArraySorted(example));
-        System.out.println("Was single-threading sorting done correctly? : " + isArraySorted(exampleCopy));
+        System.out.println("\nWas multi-threading sorting done correctly?: " + isArraySorted(example));
+        System.out.println("Was single-threading sorting done correctly?: " + isArraySorted(exampleCopy));
 
     }
 }
